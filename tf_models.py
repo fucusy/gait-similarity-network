e07b85cd086869b119792dbfe93a8e8cbb0876bc
fucusy <fucus@qq.com>
2016-09-20 16:30:41 +0800

    add validation

--- tf_models.py
+++ tf_models.py
@@ -32,7 +32,7 @@ def siamses_test_share_part(x, weights, biases):
     out = tf.nn.dropout(out, dropout)
     return out
 
-def siamses_test(data):
+def siamses_test(data, val_data):
     x1 = tf.placeholder(tf.float32, [None, 210, 70, 1])
     x2 = tf.placeholder(tf.float32, [None, 210, 70, 1])
     y = tf.placeholder(tf.float32, [None, 2])
@@ -53,26 +53,33 @@ def siamses_test(data):
     }
     left = siamses_test_share_part(x1, weights, biases)
     right = siamses_test_share_part(x2, weights, biases)
-    distance  = tf.sqrt(tf.reduce_sum(tf.pow(tf.sub(left, right),2),1,keep_dims=True))   
+    distance  = tf.sqrt(tf.reduce_mean(tf.pow(tf.sub(left, right),2),1,keep_dims=True))   
     loss = contrastive_loss(y, distance) 
     lr = 1e-3
     optimizer = tf.train.AdamOptimizer(lr).minimize(loss)
 
     init = tf.initialize_all_variables()
-    epcho = 1
+    epoch = 2
     fragment_size = 512
+    batch_count = 0
+    val_every_batch = 50
     with tf.Session() as sess:
         sess.run(init)
         data.reset_index()
-        for i in range(epcho):
+        for i in range(epoch):
             while data.have_next():
+                batch_count += 1
                 batch_x, batch_y, _ = data.next_fragment(fragment_size, need_label=True, preprocess_fuc=normalization_grey_image)
-                sess.run(optimizer, feed_dict={x1: batch_x[0], x2:batch_x[1], y: batch_y})
                 loss_val = sess.run(loss, feed_dict={x1: batch_x[0], x2:batch_x[1], y: batch_y})
-                print(loss_val)
-                print("Minibatch loss=%0.2f" % loss_val)
+                print("epoch %02d, batch count, %05d: Minibatch loss=%0.2f" % (epoch, batch_count, loss_val))
 
-
+                sess.run(optimizer, feed_dict={x1: batch_x[0], x2:batch_x[1], y: batch_y})
+                if batch_count % val_every_batch == 0:
+                    if not val_data.have_next():
+                        val_data.reset_index()
+                    batch_x, batch_y, _ = val_data.next_fragment(fragment_size, need_label=True, preprocess_fuc=normalization_grey_image)
+                    loss_val = sess.run(loss, feed_dict={x1: batch_x[0], x2:batch_x[1], y: batch_y})
+                    print("epoch %02d, val loss=%0.2f" % (i, loss_val))
 if __name__ == '__main__':
     level = logging.INFO
     FORMAT = '%(asctime)-12s[%(levelname)s] %(message)s'
@@ -84,4 +91,4 @@ if __name__ == '__main__':
 
     logging.info("train data image count %s" % train_data.count())
     logging.info("validation data image count %s" % validation_data.count())
-    siamses_test(train_data)
+    siamses_test(train_data, validation_data)
