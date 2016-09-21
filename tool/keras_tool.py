dc56822e6bd70e659b159f754485a0114cfe1f97
fucusy <fucus@qq.com>
2016-09-21 09:55:27 +0800

    transport to local machine to debug

--- tool/keras_tool.py
+++ tool/keras_tool.py
@@ -90,10 +90,9 @@ def img_preprocess(img):
     :return:
     """
     img = img.astype(np.float32)
-    mean = imread(config.Data.mean_img_file_name)
-    img -= mean
-    img = img.transpose((2, 0, 1))
-
+    img /= 255
+    if len(img.shape) == 2:
+        img = img.reshape(img.shape[0], img.shape[1], 1)
     return img
 
 def load_test_data_set(test_img_path):
@@ -108,9 +107,8 @@ def load_data(train_dirs):
     :param test_dirs:
     :return: three DataSet structure include train data, validation data, test data
     """
-    train_data, validation_data = load_train_validation_data_set(train_dirs)
-
-    return train_data, validation_data
+    train_dat, val_dat, test_dat=load_train_validation_data_set(train_dirs)
+    return train_dat, val_dat, test_dat
 
 
 
@@ -148,9 +146,14 @@ def load_train_validation_data_set(path):
     val_ids = ["%03d" % x for x in range(51, 75)]
     test_ids = ["%03d" % x for x in range(75, 124)]
 
+    condition = ["nm"]
+
     for i in range(len(img_list)):
         img_id = os.path.basename(img_list[i]).split('.')[0]
         id = img_id.split("-")[0]
+        cond = img_id.split("-")[1]
+        if cond not in condition:
+            continue
         if id in val_ids:
             validation_img_list.append(img_list[i])
             validation_img_label.append(img_label[i])
@@ -163,13 +166,12 @@ def load_train_validation_data_set(path):
 
     train_data = SimiDataSet(train_img_list, train_img_label)
     val_data = SimiDataSet(validation_img_list, validation_img_label)
-    # test_data = DataSet(test_img_list, test_img_label)
-    return train_data, val_data
+    test_data = SimiDataSet(test_img_list, test_img_label)
+    return train_data, val_data, test_data
 
 class SimiDataSet(object):
     def __init__(self, img_paths, img_labels):
         """
-
         :param img_paths:
         :param img_labels:
         :return:
@@ -180,40 +182,43 @@ class SimiDataSet(object):
         self.simi = []
         self.img_paths = img_paths
         self.img_labels = img_labels
+        self.labels = []
+        self.label_to_imgs = {}
+        self.pre_func = img_preprocess
+
         labels = set()
-        label_to_imgs = {}
         for i in range(len(img_labels)):
-            label = img_labels[i]
+            label = "%03d" % img_labels[i]
             img = img_paths[i]
             labels.add(label)
-            if label not in label_to_imgs:
-                label_to_imgs[label] = [img, ]
+            if label not in self.label_to_imgs:
+                self.label_to_imgs[label] = [img, ]
             else:
-                label_to_imgs[label].append(img)
+                self.label_to_imgs[label].append(img)
 
-        labels = sorted(labels)
-        for l in labels:
-            count_l = len(label_to_imgs[l])
+        self.labels = sorted(labels)
+        for l in self.labels:
+            count_l = len(self.label_to_imgs[l])
             for i in range(count_l):
                 for j in range(i, count_l):
-                    self.pairs[0].append(label_to_imgs[l][i])
-                    self.pairs[1].append(label_to_imgs[l][j])
+                    self.pairs[0].append(self.label_to_imgs[l][i])
+                    self.pairs[1].append(self.label_to_imgs[l][j])
                     self.simi.append(1)
 
-                    self.pairs[0].append(label_to_imgs[l][j])
-                    self.pairs[1].append(label_to_imgs[l][i])
+                    self.pairs[0].append(self.label_to_imgs[l][j])
+                    self.pairs[1].append(self.label_to_imgs[l][i])
                     self.simi.append(1)
 
-        for i in range(len(labels)):
-            j = (i+1) % len(labels)
-            l_i = labels[i]
-            l_j = labels[j]
-            count_i = len(label_to_imgs[l_i])
-            count_j = len(label_to_imgs[l_j])
+        for i in range(len(self.labels)):
+            j = (i+1) % len(self.labels)
+            l_i = self.labels[i]
+            l_j = self.labels[j]
+            count_i = len(self.label_to_imgs[l_i])
+            count_j = len(self.label_to_imgs[l_j])
             for index_i in range(count_i):
                 for index_j in range(count_j):
-                    self.pairs[0].append(label_to_imgs[l_i][index_i])
-                    self.pairs[1].append(label_to_imgs[l_j][index_j])
+                    self.pairs[0].append(self.label_to_imgs[l_i][index_i])
+                    self.pairs[1].append(self.label_to_imgs[l_j][index_j])
                     self.simi.append(0)
 
         random = 2016
@@ -227,7 +232,55 @@ class SimiDataSet(object):
         self.simi = to_categorical(self.simi, 2)
         print("after", self.simi, self.simi.shape)
 
-    def get_labels(self):
+    def get_probes_img_paths(self, hid, probe_view):
+        if hid not in self.label_to_imgs.keys():
+            return []
+        imgs = []
+        for img_path in self.label_to_imgs[hid]:
+            split_img_path = os.path.basename(img_path).split('-')
+            seq = split_img_path[2]
+            cond = split_img_path[1]
+            view = split_img_path[3]
+            if view == probe_view and cond == 'nm' and seq in ['05', '06']:
+                imgs.append(img_path)
+        return imgs
+
+    def get_probes(self, hid, view):
+        """
+        get imgs of nm-05 nm-06 of this hid and this probe_view
+        hid: string, the human id, such as '001'
+        probe_view: string, the view, such as '018'
+        return a numpy of imgs data
+        """
+        imgs = self.get_probes_img_paths(hid, view)
+        return self.img_path_2_pic(imgs)
+
+    def get_gallery_paths(self, probe_view):
+        paths = []
+        labels = []
+        for i in range(len(self.img_paths)):
+            img_p = self.img_paths[i]
+            img_l = self.img_labels[i]
+            split_img_path = os.path.basename(img_path).split('-')
+            seq = split_img_path[2]
+            cond = split_img_path[1]
+            view = split_img_path[3]
+            if cond == 'nm' and view in ['01','02','03','04']:
+                paths.append(img_p)
+                labels.append(img_l)
+        return paths, labels
+
+    def get_gallerys(self, view):
+        """
+        get imgs of nm-01 nm-02 nm-03 nm-04 of this probe_view
+        view: string, the view, such as '018'
+        return numpy of list imgs and numpy of list of hids
+        """
+        paths, labels = self.get_gallery_paths(view)
+        datas = self.img_path_2_pic(paths)
+        return datas, np.array(labels) 
+
+    def get_simi(self):
         return self.simi
 
     def count(self):
@@ -249,7 +302,7 @@ class SimiDataSet(object):
         self.set_index_in_epoch(index_in_epoch)
         return result
 
-    def next_fragment(self, fragment_size, preprocess_fuc=None, need_label=True):
+    def next_fragment(self, fragment_size, need_label=True):
         pairs = [[], []]
         start = self.index_in_epoch
         end = min(self.index_in_epoch + fragment_size, self.count())
@@ -257,13 +310,8 @@ class SimiDataSet(object):
         pairs[0] = self.pairs[0][start:end]
         pairs[1] = self.pairs[1][start:end]
 
-        feature_list = self.pairs_2_pic(pairs, preprocess_fuc)
-        for i in range(2):
-            if len(feature_list[i].shape) == 3:
-                feature_list[i] = feature_list[i].reshape(feature_list[i].shape[0], feature_list[i].shape[1], feature_list[i].shape[2], 1)
+        feature_list = self.pairs_2_pic(pairs)
 
-            print "feature_list shape"
-            print feature_list[i].shape
         img_paths = [[], []]
         for i in range(2):
             img_paths[i] = [os.path.basename(x) for x in pairs[i]]
@@ -275,17 +323,17 @@ class SimiDataSet(object):
         else:
             return feature_list, img_paths
 
-    def img_path_2_pic(self, img_path_list, func=None):
+    def img_path_2_pic(self, img_path_list):
         img_pic_list = []
         for img_path in img_path_list:
             im = imread(img_path)
-            if func is not None:
-                im = func(im)
+            if self.pre_func is not None:
+                im = self.pre_func(im)
             img_pic_list.append(im)
         return np.array(img_pic_list)
 
-    def pairs_2_pic(self, pairs, func=None):
-        return [self.img_path_2_pic(pairs[0], func), self.img_path_2_pic(pairs[1], func)]
+    def pairs_2_pic(self, pairs):
+        return [self.img_path_2_pic(pairs[0]), self.img_path_2_pic(pairs[1])]
 
 if __name__ == '__main__':
     level = logging.INFO
@@ -294,12 +342,17 @@ if __name__ == '__main__':
 
     train_img_dirs = config.data.train_img_dirs 
     print train_img_dirs 
-    train_data, validation_data = load_data(train_img_dirs)
+    train_data, validation_data, _ = load_data(train_img_dirs)
     while train_data.have_next():
-        img_list, img_label, _ = train_data.next_fragment(2, need_label=True,preprocess_fuc=normalization_grey_image)
+        img_list, img_label, _ = train_data.next_fragment(2,need_label=True)
         for n in img_list[0][0]:
             for m in n:
                 print m
         print(img_list[0][0])
         print(img_label)
+        probe_view = "018"
+        label = train_data.labels[0]
+        imgs = train_data.get_probes(label, probe_view)
+        print("imgs of view:%s label:%s" % (probe_view, label))
+        print(imgs)
         break
