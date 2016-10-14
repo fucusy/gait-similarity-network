52077ea17fcd6dc1919e28a08a075b1a91dd8159
fucusy <fucus@qq.com>
2016-10-14 22:28:03 +0800

    remove redundant configuration setting

--- config.py
+++ config.py
@@ -1,86 +1,22 @@
 __author__ = 'fucus'
 
-
-class Project:
-
-    # required, your project's absolute path, in other way, it's the absolute path for this file
-    project_path = "/home/chenqiang/github/gait-similarity-network/"
-
-    driver_img_list_path = ""
-
-    # required, this path contain the train image in sub-folder, the there are ten sub-folders, c0, c1, c2, c3 .... c9
-    train_img_folder_path = "/Users/fucus/Documents/buaa/projects/State_Farm_Distracted_Driver_Detection/data/imgs/train"
-
-    # required, this path contain imgs to be tested
-    test_img_folder_path = "/Users/fucus/Documents/buaa/projects/State_Farm_Distracted_Driver_Detection/data/imgs/test"
-
-    # the original training folder downloaded from kaggle
-    original_training_folder = ""
-
-    # the original test folder downloaded from kaggle
-    original_testing_folder = ""
-
-    # not required, a img path for you exercise program
-    test_img_example_path = "/Users/fucus/Documents/buaa/projects/State_Farm_Distracted_Driver_Detection/data/imgs/train/c0/img_27327.jpg"
-
-    # required, result output path
-    result_output_path = "./result/"
-
-    # required, save cache or not
-    save_cache = False
-
-
-    vgg_weight_file_path = "/home/chenqiang/kaggle_driver_data/vgg16_weights.h5"
-
 class data:
-    train_type = "nm" 
-    train_seq = ["01", "02", "03", "04"] 
-    train_angle = "090" 
-     
-    test_type = "nm" 
-    test_seq = ["01", "02"] 
-    test_angle = "054"
-
     train_img_dirs = ["/home/chenqiang/data/CASIA_gait_data_GEI_center_gravity"]
     test_accu = ['nm']
 
 class CNN:
-    #
-    keras_train_weight = "%s/CNN/vgg_try_karea/cache/model_weights_2_vgg_16_2x20.h5" % Project.project_path
-    # keras structure files
-    keras_structure_files = ""
-
-    fine_tuning_vgg_weight_file_path = ""
-
-    '''/path/to/json/vgg_self_exp1_keras_arch.json'''
-    model_arch_file_name = '/home/liuzheng/competition/kaggle/distractedDrivers/vgg_self_exp1_keras_arch.json'
-
-    '''/path/to/h5/vgg16_weights.h5'''
-    model_weights_file_name = '/home/liuzheng/competition/kaggle/distractedDrivers/vgg_self_exp1_keras_weights_best_vLoss0.11026_vAcc0.969.h5'
-
-    optimizer = 'sgd'
-
-    train_iter = 50
-
     test_batch_size = 64
 
     batch_size = 64
-
-    '''path/to/save/model/'''
-    model_save_path = '%s/cache' % Project.project_path
-
-    '''path/to/prediction/and/file/name/'''
-    prediction_save_file = '%s/result/prediction.csv' % Project.project_path
-
-    feature_save_path = '%s/cache' % Project.project_path
 
     load_image_to_memory_every_time = 8192
 
     load_image_to_memory_every_time_when_test = 8192
 
-    val_every = 20 # [10, 20, 40]
+    val_every = 40 # [10, 20, 40]
     lr = 1e-3
     margin = 5 # [1, 10, 100, 1000]
-    model_name = 'benchmark'
+    model_name = 'siamses_test'
     K = 5
-    output_dim = 4096 # [512, 1024, 2048, 4096]
+    output_dim = 2048 # [512, 1024, 2048, 4096]
+
